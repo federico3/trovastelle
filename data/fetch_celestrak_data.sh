7c45b4ed3af17ecc46fa0c0920691b56892dbf62
Federico Rossi (347N) <federico.rossi@jpl.nasa.gov>
2021-12-12 12:35:25 -0800

    Update: script should use data path

--- data/fetch_celestrak_data.sh
+++ data/fetch_celestrak_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget http://celestrak.com/NORAD/elements/stations.txt -O /home/pi/sky-finder/data/Celestrak_stations.txt
-wget http://celestrak.com/NORAD/elements/visual.txt -O /home/pi/sky-finder/data/Celestrak_visual.txt
+wget http://celestrak.com/NORAD/elements/stations.txt -O $CELESTIAL_COMPASS_DATA/Celestrak_stations.txt
+wget http://celestrak.com/NORAD/elements/visual.txt -O $CELESTIAL_COMPASS_DATA/Celestrak_visual.txt
 
 
