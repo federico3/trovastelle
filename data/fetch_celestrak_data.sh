2842bab05de70e41f9030e3bbc9bb373b89aa1df
Federico Rossi <hello@federico.io>
2021-12-12 20:32:26 +0000

    Add: script to fetch Celestrak data

---
+++ data/fetch_celestrak_data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget http://celestrak.com/NORAD/elements/stations.txt -O /home/pi/sky-finder/data/Celestrak_stations.txt
+wget http://celestrak.com/NORAD/elements/visual.txt -O /home/pi/sky-finder/data/Celestrak_visual.txt
+
+
