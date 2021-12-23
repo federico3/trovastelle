dc5a6a40752170de850a3c8799603ed79937c32d
Federico Rossi (347N) <federico.rossi@jpl.nasa.gov>
2021-12-23 04:23:23 -0800

    Add: skip old satellites

--- src/celestial_compass/data/satellites.py
+++ src/celestial_compass/data/satellites.py
@@ -20,21 +20,31 @@
 import os
 from skyfield.api import Loader, wgs84
 from celestial_compass.observables import ObservableSatellite
+import datetime
 
 DATA_PATH = os.environ.get("CELESTIAL_COMPASS_DATA")
 load = Loader(DATA_PATH)
+ts = load.timescale()
 
 stations_url = 'http://celestrak.com/NORAD/elements/stations.txt'
 visual_sats_url = 'https://www.celestrak.com/NORAD/elements/visual.txt'
 stations = load.tle_file(stations_url, filename="Celestrak_stations.txt")
 visual_sats = load.tle_file(visual_sats_url, filename="Celestrak_visual.txt")
 
-# TODO if epoch is too old, try to reload
+# If epoch is too old, try to reload.
+# This is now handled by the fetch_celestrak_data.sh script, symlinked in /etc/cron.daily.
 
 by_name_stations = {sat.name: sat for sat in stations}
 by_name_visual = {sat.name: sat for sat in visual_sats}
+
 satellites = [by_name_stations['ISS (ZARYA)'], by_name_stations['TIANHE'], by_name_visual['HST']]
 
 ObservableSatellites = []
+now = ts.from_datetime(datetime.datetime.now(datetime.timezone.utc))
+
 for satellite in satellites:
-    ObservableSatellites.append(ObservableSatellite(name=satellite.name, data=satellite, type_name="Satellite"))
+    tle_age_days = abs(now-satellite.epoch)
+    if tle_age_days < 14: # Relatively fresh TLEs
+        ObservableSatellites.append(ObservableSatellite(name=satellite.name, data=satellite, type_name="Satellite"))
+    else:
+        logging.warning("Satellite {} TLE is {} days old, skipping".format(satellite, tle_age_days))
